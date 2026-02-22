662edf67772adb35ded7cf969469df5ad8fb93f4
Mokuyoubi <mokuyoubi@Harutakas-MacBook-Air.local>
2026-02-22 10:51:49 +0900

    update

--- src/app.config.js
+++
@@ -1,8 +0,0 @@
-// app.config.js
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-  },
-});
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,42 +1,41 @@
-// import { createClient } from "@supabase/supabase-js";
-// import { Platform } from "react-native";
-// import "react-native-url-polyfill/auto";
-// import { storage } from "./storage";
-
-// export const supabase = createClient(
-//   process.env.EXPO_PUBLIC_SUPABASE_URL!,
-//   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
-//   {
-//     auth: {
-//       storage, // スマホならスマホの、webならwebの。
-//       persistSession: true,
-//       autoRefreshToken: true,
-//       detectSessionInUrl: Platform.OS === "web", // Webの場合のみtrue
-//     },
-//   },
-// );
-
-
-// supabase.ts
 import { createClient } from "@supabase/supabase-js";
-import Constants from "expo-constants";
 import { Platform } from "react-native";
 import "react-native-url-polyfill/auto";
 import { storage } from "./storage";
 
-const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
-const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
-
-if (!supabaseUrl) throw new Error("supabaseUrl is required");
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage,
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: Platform.OS === "web",
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      storage, // スマホならスマホの、webならwebの。
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: Platform.OS === "web", // Webの場合のみtrue
+    },
   },
-});
+);
+
+// // supabase.ts
+// import { createClient } from "@supabase/supabase-js";
+// import Constants from "expo-constants";
+// import { Platform } from "react-native";
+// import "react-native-url-polyfill/auto";
+// import { storage } from "./storage";
+
+// const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
+// const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
+
+// if (!supabaseUrl) throw new Error("supabaseUrl is required");
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+//   auth: {
+//     storage,
+//     persistSession: true,
+//     autoRefreshToken: true,
+//     detectSessionInUrl: Platform.OS === "web",
+//   },
+// });
 
 export const handleAuthCallback = async (url: string) => {
   try {
