747b629bd779c3f4247079debe41aaa05c3fdc33
Mokuyoubi <mokuyoubi@Harutakas-MacBook-Air.local>
2026-09-04 13:11:20 +0900

    update supabase

---
+++ supabase/migrations/20260904040706_update_supabase.sql
@@ -0,0 +1,20 @@
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "allow_bot_match";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "games_13";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "games_9";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "giantkill_13";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "giantkill_9";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "points_13";
+
+ALTER TABLE "private"."profiles"
+  DROP COLUMN "points_9";
